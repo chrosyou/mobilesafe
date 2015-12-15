2f70d8d29a350b50f84a2a1f0408c44cb55d17c1
chrosyou <646680299@qq.com>
2015-12-15 23:57:08 +0800

    add mobilesafe

---
+++ MobileSafe/app/src/main/java/com/example/macalyou/mobilesafe/welcompage/SplashActivity.java
@@ -0,0 +1,41 @@
+package com.example.macalyou.mobilesafe.welcompage;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.TextView;
+
+import com.example.macalyou.mobilesafe.R;
+import com.example.macalyou.mobilesafe.welcompage.utils.MyUtils;
+import com.example.macalyou.mobilesafe.welcompage.utils.VersionUpdateUtils;
+
+
+/**
+ * Created by Administrator on 2015/11/28.
+ */
+public class SplashActivity extends Activity {
+
+    /** 使用的版本号 */
+    private TextView mVersionTV;
+    /** 本地的版本号 */
+    private String mVersion;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //设置没有标题栏，在加载布局之前使用
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_slpash);
+
+        TextView version = (TextView)findViewById(R.id.welcomepage_version);
+        version.setText(MyUtils.getVersion(this));
+        final VersionUpdateUtils updateutils = new VersionUpdateUtils("de", SplashActivity.this);
+        new Thread() {
+            public void run() {
+                updateutils.getCloudVersion();
+            }
+        }.start();
+
+    }
+}
---
+++ MobileSafe/app/src/main/java/com/example/macalyou/mobilesafe/welcompage/entity/VersionEntity.java
@@ -0,0 +1,13 @@
+package com.example.macalyou.mobilesafe.welcompage.entity;
+
+/**
+ * Created by Administrator on 2015/11/28.
+ */
+public class VersionEntity {
+    /** 服务器版本号 */
+    public String versioncode;
+    /** 版本描述 */
+    public String description;
+    /** apk下载地址 */
+    public String apkurl;
+}
